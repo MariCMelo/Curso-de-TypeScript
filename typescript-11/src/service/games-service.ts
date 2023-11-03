a237af63a4616ba5d59f8aed0b348b5a2317fb1d
MariCMelo <marilia.correa.melo@gmail.com>
2023-11-03 07:19:53 -0300

    Aula 11

---
+++ typescript-11/src/service/games-service.ts
@@ -0,0 +1,28 @@
+import gamesRepository from "repositories/games-repository";
+import { CreateGame, Game } from "./../protocols/game-protocol";
+
+async function getGames(): Promise<Game[]> {
+  const games = await gamesRepository.getGames();
+  return games
+}
+
+async function createGame(game: CreateGame): Promise<void> {
+  if (await gameAlreadyExists(game)) {
+    throw { message: "Game already exists" }
+  }
+
+  await gamesRepository.createGame(game);
+}
+
+async function gameAlreadyExists(game: Game): Promise<boolean> {
+  const result = await gamesRepository.getGameByTitleAndPlatform(game.title, game.platform);
+  console.log("result", result)
+  return result.length > 0
+}
+
+const gamesService = {
+  getGames,
+  createGame
+}
+
+export default gamesService;
